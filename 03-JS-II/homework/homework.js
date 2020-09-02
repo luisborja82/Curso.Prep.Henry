ef33020d085f4f641f7d9b01a1704e723e0edaa8
luisborja82 <70439538+luisborja82@users.noreply.github.com>
2020-09-02 14:21:15 -0300

    Update homework.js

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,6 +5,20 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  var aux =0;
+  
+  if x>=aux
+  {
+    aux=x;
+    
+    
+  }
+  if y>=aux
+  {
+    aux=y;
+  }
+  
+  return aux;
 }
 
 function saludo(idioma) {
